e7a00301eb0d0ad2c364f90ed501f5c527fd12cf
giacasselli <101293161+giacasselli@users.noreply.github.com>
2022-03-17 16:35:37 -0400

    Update animationPlayer.java

--- src/finalproject/animationPlayer.java
+++ src/finalproject/animationPlayer.java
@@ -4,8 +4,32 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.lang.*;
 class Shape
 {
+    int x;
+    int y;
+    int r;
+    String colour;
+    int start;
+    int border;
+    //set default border colour in case not specified
+    String borderColour="0,255,0";
+    int length;
+    int width;
+    int startX;
+    int startY;
+    int endX;
+    int endY;
+    //made this to make sure objects were actually being created
+    //in the load animation mathod
+    static int count;
+    
+    Shape()
+    {
+        //updates everytime an object is made
+        count++;
+    }
     void Hide(int start)
     {   
         
@@ -18,13 +42,13 @@ class Shape
     {
         
     }
-    void ChangeColour(int start)
+    void ChangeColour(int start, String colour1)
     {
         
     }
 }
 class Circle extends Shape
-        {
+{
     int r;
     int x;
     int y;
@@ -32,14 +56,9 @@ class Circle extends Shape
     int border;
     String borderColour;
     
-    Circle(int r, int x, int y, String colour, int border, String borderColour)
+    Circle()
     {
-        this.r=r;
-        this.x=x;
-        this.y=y;
-        this.colour=colour;
-        this.border=border;
-        this.borderColour=borderColour;
+        
     }
     @Override
     void Hide(int start)
@@ -57,9 +76,9 @@ class Circle extends Shape
         super.Jump(start, x, y);
     }
     @Override
-    void ChangeColour(int start)
+    void ChangeColour(int start, String colour1)
     {
-        super.ChangeColour(start);
+        super.ChangeColour(start, colour1);
     }
 }
 class Rectangle extends Shape
@@ -72,15 +91,11 @@ class Rectangle extends Shape
     int length;
     int width;
     
-    Rectangle(int x, int y, int border, String colour, String borderColour, int length, int width)
+    Rectangle()
     {
-        this.x=x;
-        this.y=y;
-        this.border=border;
-        this.colour=colour;
-        this.borderColour=borderColour;
-        this.length=length;
-        this.width=width;
+        //debug
+        //System.out.print(count);
+
     }
     @Override
     void Hide(int start)
@@ -98,9 +113,9 @@ class Rectangle extends Shape
         super.Jump(start, x, y);
     }
     @Override
-    void ChangeColour(int start)
+    void ChangeColour(int start, String colour1)
     {
-        super.ChangeColour(start);
+        super.ChangeColour(start, colour1);
     }
 }
 class Line extends Shape
@@ -112,14 +127,9 @@ class Line extends Shape
     String colour;
     int border;
     
-    Line(int startX, int startY, int endX, int endY, String colour, int border)
+    Line()
     {
-        this.startX=startX;
-        this.startY=startY;
-        this.endX=endX;
-        this.endY=endY;
-        this.colour=colour;
-        this.border=border;
+
     }
     @Override
     void Show(int start)
@@ -137,29 +147,189 @@ class Line extends Shape
         super.Jump(start, x, y);
     }
     @Override
-    void ChangeColour(int start)
+    void ChangeColour(int start, String colour1)
     {
-        super.ChangeColour(start);
+        super.ChangeColour(start, colour1);
     }
 }
 class ap
 {
+    int frames;
+    int speed;
+    int numObjs;
     void loadAnimationFromFile(String FileName)
     {
+        //there a LOT
+        //im gonna comment in case it gets too confusing
+        //text me if something doesnt make sense
+        
+        //create new file to be read
         File newfile= new File(FileName);
+        //create empty string to read the file to
         String data="";
         try {
+            //read all lines of the file to the string
             Scanner reader=new Scanner(newfile);
             while(reader.hasNextLine())
             {
                 data+=reader.nextLine();
                 data+="\n";
             }
+            //catch if file is not found
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ap.class.getName()).log(Level.SEVERE, null, ex);
         }
+        //debug
         System.out.print(data);
+        //create array of strings from original data file
+        //this file will be separated by two new line characters
+        //i.e. the specs of the animation, and each object in the animation
+        String[] objs=data.split("\n\n");
+        //the specs of the animation will be put into its own array of strings
+        //so data can be extracted
+        //split at new line character
+        String[] specs=objs[0].split("\n");
+        //total number of frames is always first line
+        frames=Integer.parseInt(specs[0].substring(8,'\n'+1));
+        //speed is always second
+        speed=Integer.parseInt(specs[1].substring(7,'\n'-1));
+        //total number of objects is always third
+        numObjs=Integer.parseInt(specs[2].substring(0));
+        //System.out.print(numObjs);
+        //now that the specs data has been extracted, move on to 
+        //the actual objects data
         
+        //create array of objects of type shape
+        //this is the parent class so each category of shape
+        //can be specified when data is read
+        Shape[] shapes;
+        //size of shape array is the number of objects as specified
+        //in the specs data
+        shapes=new Shape[numObjs];
+        //this is where it gets confusing, bare with me
+        
+        //create an array of arrays of strings
+        //first index represents object number
+        //second index is line number within that objects info block in .txt file
+        String[][]info=new String[numObjs][];
+        int i;
+        //split each object into array of strings
+        //split by new line character
+        for(i=1;i<=numObjs;i++)
+        {
+            info[i-1]=objs[i].split("\n");
+        }
+        //debug
+        //System.out.print(info[1][0]);
+        
+        //now, we create the objects and put them into the shapes array
+        for(i=0;i<numObjs;i++)
+        {
+            //since first line is always blank, second index starts at 1, not 0
+            switch(info[i][1])
+            {
+                case("Circle"):
+                    shapes[i]=new Circle();
+                    break;
+                case("Rect"):
+                    shapes[i]=new Rectangle();
+                    break;
+                case("Line"):
+                    shapes[i]=new Line();
+                    break;
+                default:
+                    break;
+            }
+                    
+        }
+        //now, we extrapolate the data on a case by case basis
+        int j;
+        //loop to index through the number of shapes
+        for(i=0;i<numObjs;i++)
+        {
+            //second loop to index through the individual line of info
+            //for each shape
+            //loop index starts at 2 to skip unnecessary info
+            for(j=2;j<info[i].length;j++)
+            {
+                //cases for different info types
+                //assign info to shape object
+                if((info[i][j].contains("r:"))&&!(info[i][j].contains("c"))&&!(info[i][j].contains("b")))
+                {
+                    shapes[i].r=Integer.parseInt(info[i][j].substring(3));
+                }
+                else if(info[i][j].contains("colour:"))
+                {
+                    shapes[i].colour=info[i][j].substring(8);
+                }
+                else if(info[i][j].contains("Hide"))
+                {
+                    shapes[i].Hide(Integer.parseInt(info[i][j+1].substring(7)));
+                    j+=2;
+                }
+                else if(info[i][j].contains("Show"))
+                {
+                    shapes[i].Show(Integer.parseInt(info[i][j+1].substring(7)));
+                    j+=2;
+                }
+                else if(info[i][j].contains("Jump"))
+                {
+                    shapes[i].Jump(Integer.parseInt(info[i][j+1].substring(7)),Integer.parseInt(info[i][j+2].substring(3)),Integer.parseInt(info[i][j+3].substring(3)));
+                    j+=4;
+                }
+                else if(info[i][j].contains("Change"))
+                {
+                    shapes[i].ChangeColour(Integer.parseInt(info[i][j+1].substring(7)),info[i][j+2].substring(8));
+                    j+=3;
+                }
+                else if(info[i][j].contains("x:"))
+                {
+                    shapes[i].x=Integer.parseInt(info[i][j].substring(3));
+                }
+                else if(info[i][j].contains("y:"))
+                {
+                    shapes[i].y=Integer.parseInt(info[i][j].substring(3));
+                }
+                else if(info[i][j].contains("effect"))
+                {
+                    j++;
+                }
+                else if(info[i][j].contains("length"))
+                {
+                    shapes[i].length=Integer.parseInt(info[i][j].substring(8));
+                }
+                else if(info[i][j].contains("width"))
+                {
+                    shapes[i].width=Integer.parseInt(info[i][j].substring(7));
+                }
+                else if((info[i][j].contains("border"))&&!(info[i][j].contains("l")))
+                {
+                    shapes[i].border=Integer.parseInt(info[i][j].substring(8));
+                }
+                else if((info[i][j].contains("border"))&&(info[i][j].contains("l")))
+                {
+                    shapes[i].borderColour=info[i][j].substring(15);
+                }
+                else if(info[i][j].contains("startX"))
+                {
+                    shapes[i].startX=Integer.parseInt(info[i][j].substring(8));
+                }
+                else if(info[i][j].contains("startY"))
+                {
+                    shapes[i].startY=Integer.parseInt(info[i][j].substring(8));
+                }
+                else if(info[i][j].contains("endX"))
+                {
+                    shapes[i].endX=Integer.parseInt(info[i][j].substring(6));
+                }
+                else if(info[i][j].contains("endY"))
+                {
+                    shapes[i].endY=Integer.parseInt(info[i][j].substring(6));
+                }
+            }
+        }
+        //debug
+        //System.out.print(shapes[1].border);
     }
     void run()
     {
